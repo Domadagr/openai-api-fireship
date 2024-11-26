7a5f4ed7ce095a1f0835cd5979c6e13dd558986a
HelDraugar <91600617+HelDraugar@users.noreply.github.com>
2024-11-26 21:05:45 +0100

    course almost done

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import OpenAI from 'openai';
+const openai = new OpenAI();
+
 import express from 'express';
 import cors from 'cors';
-
-import OpenAI from "openai";
-const openai = new OpenAI();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/domadagr', async (req, res) => {
+app.post('/dream', async (req, res) => {
     const prompt = req.body.prompt;
 
-    const response = await openai.images.generate({
+    const aiResponse = await openai.images.generate({
         prompt,
         n: 1,
-        size: "1024x1024",
-      });
-      const image_url = response.data[0].url;
-      res.send =({ image_url });
-      console.log(image_url);
+        size: '1024x1024',
+    });
 
+    const image = aiResponse.data[0].url;
+    res.send({ image });
 });
 
-app.listen(8080, () => console.log('Domadagr dreams http://localhost:8080/domadagr'))
+app.listen(8080, () => console.log('make art on http://localhost:8080/domadagr'));
