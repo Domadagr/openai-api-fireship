dab41bac5e761956aaa94b8d014b053933661bc8
HelDraugar <91600617+HelDraugar@users.noreply.github.com>
2024-11-26 16:54:43 +0100

    cleaned up server.js and fixed api call, tested in thunder client. Started frontend html

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import express from 'express';
+import cors from 'cors';
+
 import OpenAI from "openai";
 const openai = new OpenAI();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const response = await openai.images.generate({
-    model: "dall-e-2",
-    prompt: "a white siamese cat",
-    n: 1,
-    size: "1024x1024",
-  });
-  image_url = response.data[0].url;
-  console.log(image_url);
+app.post('/domadagr', async (req, res) => {
+    const prompt = req.body.prompt;
+
+    const response = await openai.images.generate({
+        prompt,
+        n: 1,
+        size: "1024x1024",
+      });
+      const image_url = response.data[0].url;
+      res.send =({ image_url });
+      console.log(image_url);
+
+});
+
+app.listen(8080, () => console.log('Domadagr dreams http://localhost:8080/domadagr'))
